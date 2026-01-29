51cfa80577f5217d96c2a7b3728c72aa17514d39
kp3166048-glitch <kp3166048@gmail.com>
2026-01-30 00:52:54 +0530

    Add MyJava class with main method

---
+++ sm.java
@@ -0,0 +1,7 @@
+public class MyJava{
+  public static void main(String[] args){
+    String str="Hello world my !";
+    int x = str.length();
+    System.out.println("string length of :" + x);
+  }
+}
