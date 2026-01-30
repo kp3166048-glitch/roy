fc7a18a3edee50035416e6e5f9252834edb35fad
kp3166048-glitch <kp3166048@gmail.com>
2026-01-30 05:43:27 +0530

    Update sm.java

--- sm.java
+++ sm.java
@@ -1,6 +1,6 @@
 public class sm{
   public static void main(String[] args){
-    String str="Hello world!";
+    String str="Hello wor";
     int x = str.length();
     System.out.println("string length of :" + x);
   }
